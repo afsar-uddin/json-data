5a0cbf8ec815862d3a4089d2889e173b43c73510
Afsar Uddin <afsarwebdev@gmail.com>
2022-01-23 14:19:37 +0600

    ecom skelyton

---
+++ portfolio.js
@@ -0,0 +1,56 @@
+
+portfolio: [
+    {
+        "title": "Led Night LIght",
+        "shortDsc": "This project based ReactJS and MongoDB for practice purpose",
+        "cover": "https://raw.githubusercontent.com/afsar-uddin/githubPhotos/main/afsar-portfolio/ledNightLight.png",
+        "usedTech": ['ReactJS', 'MongoDB', 'ExpressJS', 'React-Bootstrap', 'React-icons', 'JSX', 'CSS'],
+        "liveSite": 'https://led-night-light.firebaseapp.com/',
+        "clientSiteRepo": "https://github.com/afsar-uddin/led-night-light",
+        "serverSiteRepo": "https://github.com/afsar-uddin/led-night-light-server",
+        "screenshots": [
+            "https://raw.githubusercontent.com/afsar-uddin/githubPhotos/main/afsar-portfolio/led/ledHome.png",
+            "https://raw.githubusercontent.com/afsar-uddin/githubPhotos/main/afsar-portfolio/led/ledAdmin.png",
+            "https://raw.githubusercontent.com/afsar-uddin/githubPhotos/main/afsar-portfolio/led/ledRevirew.png"
+        ]
+    },
+    {
+        "title": "Europe Tour",
+        "shortDsc": "This project based ReactJS and MongoDB for practice purpose",
+        "cover": "https://raw.githubusercontent.com/afsar-uddin/githubPhotos/main/afsar-portfolio/europTour.png",
+        "usedTech": ['ReactJS', 'MongoDB', 'ExpressJS', 'React-Bootstrap', 'React-icons', 'JSX', 'CSS'],
+        "liveSite": 'https://europe-tour-explore.web.app/',
+        "clientSiteRepo": "https://github.com/afsar-uddin/europe-tour",
+        "serverSiteRepo": "https://github.com/afsar-uddin/europe-tour-server",
+        "screenshots": [
+            "https://raw.githubusercontent.com/afsar-uddin/githubPhotos/main/afsar-portfolio/europe/europoeHome.png",
+            "https://raw.githubusercontent.com/afsar-uddin/githubPhotos/main/afsar-portfolio/europe/uropeTrips.png",
+            "https://raw.githubusercontent.com/afsar-uddin/githubPhotos/main/afsar-portfolio/europe/europeAddTrips.png"
+        ]
+    },
+    {
+        "title": "Mark Tweedy",
+        "shortDsc": "This site based on WordPress for my client",
+        "cover": "https://raw.githubusercontent.com/afsar-uddin/githubPhotos/main/afsar-portfolio/mark.png",
+        "usedTech": ['PHP', 'WordPress', 'Custom Post', 'HTML5', 'CSS3', 'Media Query'],
+        "liveSite": 'http://marktweedy.com/',
+        "screenshots": [
+            "https://raw.githubusercontent.com/afsar-uddin/githubPhotos/main/afsar-portfolio/markTweedy/mHome.png",
+            "https://raw.githubusercontent.com/afsar-uddin/githubPhotos/main/afsar-portfolio/markTweedy/mServices.png",
+            "https://raw.githubusercontent.com/afsar-uddin/githubPhotos/main/afsar-portfolio/markTweedy/mTestimonial.png"
+        ]
+    },
+    {
+        "title": "Honda CBR",
+        "shortDsc": "This site based on HTML and CSS",
+        "cover": "https://raw.githubusercontent.com/afsar-uddin/githubPhotos/main/afsar-portfolio/honda-cbr.png",
+        "usedTech": ['HTML and HTML5', 'CSS and CSS3', 'Media Query', 'Animation'],
+        "liveSite": 'https://honda-cbr-bootstra p-assignment-afsar-uddin.netlify.app/',
+        "clientSiteRepo": "https://github.com/afsar-uddin/honda-cbr",
+        "screenshots": [
+            "https://raw.githubusercontent.com/afsar-uddin/githubPhotos/main/afsar-portfolio/cbr/cbrHome.png",
+            "https://raw.githubusercontent.com/afsar-uddin/githubPhotos/main/afsar-portfolio/cbr/cbr2.png",
+            "https://raw.githubusercontent.com/afsar-uddin/githubPhotos/main/afsar-portfolio/cbr/cbr3.png"
+        ]
+    }
+]
